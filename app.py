db7134d5a3a88dbb704e4e5c50447fd2ad28ab71
dmitriyignatiev <dmitriy.ignatiev83@gmail.com>
2019-07-04 16:33:58 +0300

    Add files via upload
    last

--- app.py
+++ app.py
@@ -3,40 +3,32 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from config import Config
 from flask_migrate import Migrate
-from flask_login import LoginManager, UserMixin, login_user, current_user, login_required,logout_user
+
+import pymysql
 from datetime import datetime
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
 migrate = Migrate(app, db)
-login_manager = LoginManager()
-login_manager.init_app(app)
 
+
+# app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:Pangolin2208@localhost:8537/fin_test_1'
 app.config.from_object(Config)
 
-
-@app.route('/finance')
-@login_required
-def finance():
+@app.route('/')
+def index():
     finance = Main.query.all()
-    return render_template('finance.html', finance=finance)
-
-
-reqs = db.Table('reqs',
-             db.Column('main_id', db.Integer, db.ForeignKey('main.id')),
-             db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
-             )
+    return render_template('index.html', finance=finance)
 
 
 class Main(db.Model):
-
     id = db.Column(db.Integer, primary_key=True)
     tora_red = db.Column(db.String(120))
     direction = db.Column(db.String(120))
     sale = db.Column(db.String(120))
     buyer = db.Column(db.String(120))
-    status_of_request = db.Column(db.String(120))
+    status_of_request=db.Column(db.String(120))
     blank_option_1 = db.Column(db.String(120))
     customer_name = db.Column(db.String(120))
     customer_accept_invoice_status = db.Column(db.String(120))
@@ -44,6 +36,7 @@ class Main(db.Model):
     customer_order_date = db.Column(db.DateTime)
     customer_order_monnth = db.Column(db.Integer)
     loading_place = db.Column(db.String(120))
+    loading_date = db.Column(db.DateTime)
     cargo_character = db.Column(db.String(120))
     unloading_place = db.Column(db.String(120))
     unloading_date = db.Column(db.DateTime)
@@ -57,12 +50,17 @@ class Main(db.Model):
     s_prepaid_amount = db.Column(db.Integer)
     s_prepaid_data = db.Column(db.DateTime)
     cost_with_vat = db.Column(db.Integer)
+    cost_pochta = db.Column(db.Integer)
+    cost_final = db.Column(db.Integer)
     cost_we_still_need_pay = db.Column(db.Integer)
+
     s_inv_status_payment = db.Column(db.String(120))
     s_inv_credit_terms = db.Column(db.Integer)
     s_credit_terms_scan_org = db.Column(db.String(120))
     s_inv_pay_intill_data = db.Column(db.DateTime)
-    s_inv_was_payment = db.Column(db.String(120))  # ЗНАЧ!!!
+    s_inv_status = db.Column(db.String(120)) # ЗНАЧ!!!
+    blank_option_2 = db.Column(db.String(120))
+
     s_inv_act_date_payment = db.Column(db.DateTime)
     s_invoice_transit_name = db.Column(db.String(120))
     c_inv_number = db.Column(db.String(120))
@@ -71,8 +69,8 @@ class Main(db.Model):
     c_inv_currency = db.Column(db.String(120))
     c_invfacture_number = db.Column(db.String(120))
     c_invfacture_data = db.Column(db.DateTime)
-    c_inv_issue = db.Column(db.String(120))  # 42
-    c_inv_track_number = db.Column(db.String(120))
+    c_inv_issue = db.Column(db.String(120))#42
+    c_ems_tracking = db.Column(db.String(120))
     c_inv_post_send_data = db.Column(db.DateTime)
     c_inv_plan_pay = db.Column(db.DateTime)
     c_inv_week_plan_pay = db.Column(db.Integer)
@@ -85,9 +83,9 @@ class Main(db.Model):
     s_np_ati = db.Column(db.String(120))
     s_inv_part_payment_16wk = db.Column(db.Integer)
     s_inv_part_pay_issue = db.Column(db.String(120))
+
     s_inv_part_payment_17wk = db.Column(db.Integer)
-    s_inv_part_payment_18wk_2019 = db.Column(db.Integer)
-    s_inv_part_payment_19wk_2019 = db.Column(db.Integer)
+    s_inv_part_payment_18wk_2019= db.Column(db.Integer)
     s_inv_part_payment_20wk_2019 = db.Column(db.Integer)
     s_inv_part_payment_21wk_2019 = db.Column(db.Integer)
     s_inv_part_payment_22wk_2019 = db.Column(db.Integer)
@@ -95,104 +93,128 @@ class Main(db.Model):
     s_inv_part_payment_24wk_2019 = db.Column(db.Integer)
     s_inv_part_payment_25wk_2019 = db.Column(db.Integer)
     s_inv_part_payment_26wk_2019 = db.Column(db.Integer)
-    s_inv_part_payment_27wk_2019 = db.Column(db.Integer)
-    s_inv_part_payment_28wk_2019 = db.Column(db.Integer)
+    s_inv_part_payment_27wk_2019= db.Column(db.Integer)
+    s_inv_part_payment_28wk_2019= db.Column(db.Integer)
+    blank_option_3 = db.Column(db.String(120))
     s_inv_part_payment_29wk_2019 = db.Column(db.Integer)
-    s_inv_part_payment_30wk_2019 = db.Column(db.Integer)
-    s_inv_part_payment_31wk_2019 = db.Column(db.Integer)
-    s_inv_part_payment_32wk_2019 = db.Column(db.Integer)
-    s_inv_part_payment_33wk_2019 = db.Column(db.Integer)
-    s_inv_part_payment_34wk_2019 = db.Column(db.Integer)
-    s_inv_part_payment_35wk_2019 = db.Column(db.Integer)
-    s_inv_part_payment_36wk_2019 = db.Column(db.Integer)
-    s_inv_part_payment_37wk_20119 = db.Column(db.Integer)
-    s_inv_part_payment_38wk_2019 = db.Column(db.Integer)
+    blank_option_4 = db.Column(db.String(120))
+    s_inv_part_march_2019 = db.Column(db.Integer)
+    blank_option_5 = db.Column(db.String(120))
+    blank_option_6 = db.Column(db.String(120))
+
     posts = db.relationship('Post', backref='fin')
-    user = db.relationship('User',
-                           secondary=reqs,
-                           backref=db.backref('user',
-                                              lazy='dynamic')
-                           )
+    user = db.relationship('User', backref='user')
 
 
 
 
-
+#exi
+#
+#
+#
 
 class Post(db.Model):
-    __tablename__ = 'post'
+
+    __tablename__='post'
 
     id = db.Column(db.Integer, primary_key=True)
     post = db.Column(db.Text)
     fin_id = db.Column(db.Integer, db.ForeignKey('main.id'))
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 
+class User(db.Model):
 
-class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), unique=True)
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(120))
     fin_id = db.Column(db.Integer, db.ForeignKey('main.id'))
-    posts = db.relationship('Post', backref='user')
-
-
 
     def __repr__(self):
         return '<User {}>'.format(self.name)
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return 'You are now logged out'
-
-@app.route('/')
-def index():
-    user = User.query.filter_by(name='Dima').first()
-    login_user(user)
-    return 'You are logged in'
-
-@app.route('/details/<int:request_n>', methods=['POST', 'GET'])
-def details(request_n):
-    fin = Main.query.get(request_n)
-    posts = Post.query.filter(Post.fin_id==request_n)
-
-    return render_template('details.html', fin=fin, posts=posts)
-
-
-
 
 @app.shell_context_processor
 def make_shell_context():
-    return {
-        'db': db,
-        'User': User,
-        'Post': Post,
-        'Main': Main,
-        'datetime':datetime,
-        'dima':User.query.get(1),
-        'vova':User.query.get(1),
-        'req1':Main.query.get(1),
-
-
+    return{
+        'db':db,
+        'User':User,
+        'Post':Post,
+        'Main':Main,
     }
-
 
 @app.route('/process', methods=['POST'])
 def process():
-    name = request.form['name']
+    name = request.form.get('name')
     print(name)
     if name:
         new = Main(customer_name=name)
         db.session.add(new)
         db.session.commit()
-        return jsonify({'name': new.id})
-    return jsonify({'error': 'Missing data'})
+        return jsonify({'name': new.customer_name})
+    return jsonify({'error':'Missing data'})
 
 
-if __name__ == '__main__':
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__=='__main__':
     app.run(debug=True, port=1010)
+
